ee7d95490f1d99f1cb3ddf5797c9499a38e86b8b
Andrew Chen <chen4192@purdue.edu>
2025-04-08 18:45:17 -0400

    Removed unused function from stats functions

--- src/functions/dashboard-stats-functions.js
+++ src/functions/dashboard-stats-functions.js
@@ -1,22 +1,6 @@
 import { getAuthThenUser } from "./dashboard-login-functions.js";
 import { getUseCounts, getTxAmounts, getKeysByUser } from "./dynamo-functions.js"
-import { badRequestResponse, createdResponse, notFoundResponse, successResponse, unauthorizedResponse } from "./responses.js";
-
-// Get Use
-export const generateApiKey = async (event) => {
-    console.log("Received generate key request");
-    const user = await getAuthThenUser(event);
-    if (!user) return unauthorizedResponse("Unauthorized");
-    const { name } = JSON.parse(event.body);
-    if (!name) return badRequestResponse("Key name required");
-
-    const userKeys = await getKeysByUser(user.id);
-    if (userKeys?.some(apiKey => apiKey.name === name))
-        return badRequestResponse("Key name already in use!");
-    const newKey = crypto.randomBytes(32).toString("hex")
-    await createKey(newKey, user.id, name, user.wallets.length ? user.wallets[0].name : null)
-    return createdResponse({ apiKey: newKey });
-};
+import { notFoundResponse, successResponse, unauthorizedResponse } from "./responses.js";
 
 // Get use counts route
 export const getUsageHistory = async (event) => {
